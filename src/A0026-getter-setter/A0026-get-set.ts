b0818ee41dff4602e6e5abef45856e770f17c6b8
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-09 14:35:07 -0300

    set/get

---
+++ src/A0026-getter-setter/A0026-get-set.ts
@@ -0,0 +1,29 @@
+export class Person {
+    constructor (
+        private name: string,
+        private nickname: string, 
+        private age: number, 
+        private cpf: string,
+    ) {};
+
+    public getAge (): number {
+        return this.age;
+    }
+
+    public setCPF (value: string): void {
+        this.cpf = value;
+    }
+
+    public getCPF (): string {
+        return this.cpf.replace(/\D/g, '');
+    }
+
+    public getfullName (): string {
+        return this.name + this.nickname;
+    }
+
+}
+
+const someGuy = new Person('David', 'Guetta', 10, '123-456-798-00');
+someGuy.setCPF('123-456-798-00');
+console.log(someGuy.getCPF());
