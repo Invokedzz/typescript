5ab0f16a165ba3a24b3a3d02fba78cfd6e3ada53
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-12 18:15:07 -0300

    hmmm analysis

--- src/A0000-overall.ts/averall.ts
+++ src/A0000-overall.ts/averall.ts
@@ -1,8 +1,65 @@
-function gofu () {
-    return function (n: number) {
-        console.log(n);
-        n++;
+export class randomDude {
+    private _weapon: Weapon | null = null;
+    constructor (
+        private _name: string,
+        private _username: string, // O underline é para métodos GET/SET.
+    ) {};
+
+    get nameValue (): string {
+        return this._name;
+    }
+
+    get usernameValue (): string {
+        return this._username;
+    }
+
+    get weaponData (): Weapon | null {
+        return this._weapon;
+    }
+
+    set weaponData (weapon: Weapon | null) {
+        this._weapon = weapon;
+    }
+
+    damageOutput (): void {
+        if (this.weaponData === null) {
+            console.log("You must register a valid weapon");
+            return;
+        }
+
+        this.weaponData.damageOutput();
+
+    }
+
+}
+
+export abstract class Weapon {
+    constructor (
+        private _weaponName: string,
+    ) {};
+
+    abstract damageOutput (): void;
+
+    get analysisWeaponValue (): string {
+        return this._weaponName;
+    }
+
+}
+
+export class Pickaxe extends Weapon {
+    damageOutput(): void {
+        const pickaxeValue = 10;
+        console.log(`The damage of your pickaxe is: ${pickaxeValue}`);
     }
 }
 
-gofu();
+
+const definingClass = new randomDude('Harry', 'Potter');
+const pickaxe = new Pickaxe('Pickaxe');
+console.log(pickaxe.analysisWeaponValue);
+pickaxe.damageOutput();
+//console.log(definingClass.nameValue); // Não acessamos GET dessa forma: "definingClass.nameValue()";
+//console.log(definingClass.usernameValue);
+
+
+export default 1;
