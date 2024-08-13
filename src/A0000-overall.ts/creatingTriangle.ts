9c748774f735c7d38ab3b7cef6b20b9757aa6455
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-13 12:27:00 -0300

    creating something funny

---
+++ src/A0000-overall.ts/creatingTriangle.ts
@@ -0,0 +1,30 @@
+export class Egyptshit {
+    constructor (
+        private element: string,
+    ) {};
+
+    public letsBuildit (): void {
+        let limit = 20;
+        for (let weloveVasco = 1; weloveVasco <= limit; weloveVasco += 1) {
+            let paranoidAndroid = ' '.repeat(limit - weloveVasco);
+            let darkStar = '*'.repeat(weloveVasco * 2 - 1);
+            let agroupThem = paranoidAndroid + darkStar;
+            console.log(agroupThem);
+        }
+    };
+
+    public creatingtheInverse () {
+        let limitBelow = 20;
+        for (let wehateVasco = limitBelow; wehateVasco >= 0; wehateVasco -= 1) {
+            let noSurprises = ' '.repeat(limitBelow - wehateVasco);
+            let plasticTrees = '*'.repeat(wehateVasco * 2 - 1);
+            let agroupAgain = noSurprises + plasticTrees;
+            console.log(agroupAgain);
+        }
+    }
+
+};
+
+const triangleIlluminati = new Egyptshit('');
+triangleIlluminati.letsBuildit();
+triangleIlluminati.creatingtheInverse();
