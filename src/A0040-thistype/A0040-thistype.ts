eb5f8fd5f0578d2e2015319282e16658e6589c78
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-19 10:40:49 -0300

    this type, duh

--- src/A0040-thistype/A0040-thistype.ts
+++ src/A0040-thistype/A0040-thistype.ts
@@ -1,9 +1,61 @@
-function removeDuplicates (nums: number []): number [] {
-    const limit = 5;
-    for (let i = 0; i <= limit; i++) {
-        if (nums) nums.pop();
-        nums.push(i);
-    }
-    return nums;
+export class Calculator {
+    constructor (
+        public num: number,
+    ) {};
+
+    add (n: number): this {
+        this.num += n;
+        return this;
+    };
+
+    subtraction (n: number): this {
+        this.num -= n;
+        return this;
+    };
+
+    multiplication (n: number): this {
+        this.num *= n;
+        return this;
+    };
+
+    division (n: number): this {
+        this.num /= n;
+        return this;
+    };
+
 }
-console.log(removeDuplicates([1, 2, 3, 4, 4, 5]));
+
+export class subCalc extends Calculator {
+    power (n: number): this {
+        this.num **= n;
+        return this;
+    }
+}
+const calcu = new subCalc(10);
+console.log(calcu.add(5).multiplication(2).division(2).power(2));
+
+// Chamar em cadeia só é possível por conta do "this";
+
+class doingRequest {
+    private method: 'get' | 'post' | null = null;
+    private url: string | null = null;
+    prepareMethod (method: 'get' | 'post'): this {
+        this.method = method;
+        return this;
+    };
+
+    prepareUrl (url: string): this {
+        this.url = url;
+        return this;
+    };
+
+    sendingData (): boolean | void {
+        if (!this.method || !this.url) return false;
+        console.log(`Requesting ${this.method} to ${this.url}`);
+    };
+}
+
+
+const requestDone = new doingRequest();
+requestDone.prepareUrl('http://localhost:3000').prepareMethod('post').sendingData();
+
