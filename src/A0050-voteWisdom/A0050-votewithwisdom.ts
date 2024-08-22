8a54a51b978c13ba3d0bcb3ae4fd6fa13a591cfe
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-22 16:48:29 -0300

    voting session

---
+++ src/A0050-voteWisdom/A0050-votewithwisdom.ts
@@ -0,0 +1,67 @@
+export class votingTime {
+    constructor (
+        private arrayPublic: string [],
+        private counter: number,
+        private counter2: number,
+    ) {};
+
+    public letsVote (participants: string []): string [] | boolean {
+        this.arrayPublic = [];
+        let limit = 5;
+        for (let i = 0; i <= limit; i++) {
+            if (i >= limit) break;
+            if (this.arrayPublic.includes(participants[i])) return false;
+            this.arrayPublic.push(participants[i]);
+            console.log(this.arrayPublic);
+        };
+
+        return this.arrayPublic;
+
+    }
+
+    public firstOption (firstChallenger: string, value: number): boolean | number {
+        if (this.arrayPublic.length > 0) {
+            const numbersofVote = value;
+            for (let i = 0; i <= numbersofVote; i++) {
+                this.counter += 1;
+            };
+            console.log(`${firstChallenger} has a total of ${this.counter} votes`);
+            return this.counter;
+
+        };
+        
+        return false;
+    };
+
+    public secondOption (secondChallenger: string, value: number): boolean | number {
+        if (this.arrayPublic.length > 0) {
+            const numbersofVote2 = value;
+            for (let i = 0; i <= numbersofVote2; i++) {
+                this.counter2 += 1;
+            };
+
+            console.log(`${secondChallenger} has a total of ${this.counter2} votes`);
+            return this.counter2;
+
+        };
+
+        return false;
+
+    };
+
+
+    public analyseCounters (firstChallenger: string, secondChallenger: string): string {
+        if (this.counter > this.counter2) return `${firstChallenger} is the winner!`;
+        return `${secondChallenger} is the winner!`;
+    };
+
+
+}
+
+const callClass = new votingTime([], 0, 0);
+callClass.letsVote(['Peter', 'John', 'Pablo', 'Ana', 'Ana', 'Orleans']);
+callClass.firstOption('Joe Biden', 1000000);
+callClass.secondOption('Donald Trump', 1000002);
+console.log(callClass.analyseCounters('Joe Biden', 'Donald Trump'));
+
+export default 1;
