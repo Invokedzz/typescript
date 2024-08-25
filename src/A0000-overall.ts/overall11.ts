df71543bca4d4276be6139d6e37e184b804689ba
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-25 16:48:54 -0300

    Update overall11.ts

--- src/A0000-overall.ts/overall11.ts
+++ src/A0000-overall.ts/overall11.ts
@@ -4,5 +4,32 @@ const changeValue = obtainNumber.toString() // Transforma nosso valor em string;
 .map(value => parseInt(value)) // Volta a transformação em inteiro;
 .reduce((ac, val) => ac + val, 0); // Soma todos os termos (2, 4, 7, 8). Resultado = 21.
 
+export class verifyNumbers {
+    public addValue (a: bigint): number {
+        return a
+        .toString()
+        .split('')
+        .map(ourValue => parseInt(ourValue, 10))
+        .reduce((ac, val) => ac + val, 0);
+    };
 
-console.log(changeValue);
+    public testMax (maxX: number, maxY: number): number {
+        let maxValue = 0;
+        for (let a = 1; a <= maxX; a++) {
+            for (let b = 1; b <= maxY; b++) {
+                const newValue = BigInt(a) ** BigInt(b);
+                const checkValue = this.addValue(newValue);
+                if (checkValue > maxValue) maxValue = checkValue;
+            };
+        };
+
+        return maxValue;
+
+    };
+
+}
+
+const maxX = 200;
+const maxY = 200;
+const getClass = new verifyNumbers();
+console.log(getClass.testMax(maxX, maxY));
