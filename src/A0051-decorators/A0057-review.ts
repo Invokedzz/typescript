38dc66088fb6bd78a8d3be981721c7dd1420b801
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-26 14:42:39 -0300

    numbers

--- src/A0051-decorators/A0057-review.ts
+++ src/A0051-decorators/A0057-review.ts
@@ -28,7 +28,7 @@ export class valueNumber {
 
 const startStringclass = new valueString('Don Quixote');
 const startNumberclass = new valueNumber('Horse');
-console.log(addParams(startStringclass.returnName(), startNumberclass.returnNum()));
+// console.log(addParams(startStringclass.returnName(), startNumberclass.returnNum()));
 
 function whyimAlive (target: string []): string {
     if (!target) return 'no target';
@@ -48,6 +48,59 @@ export class becausetheresaPurpose {
 }
 
 const newPurpose = new becausetheresaPurpose(['John', 'Bill']);
-console.log(whyimAlive(newPurpose.returnTarget()));
+// console.log(whyimAlive(newPurpose.returnTarget()));
+
+function doingMath (firstValue: number, secondValue: number): number {
+    if (!firstValue && !secondValue) return 0;
+    return (firstValue + secondValue) / 2;
+}
+
+export class firstAttribute {
+    constructor (
+        private firstValue: number,
+    ) {};
+
+    public returningValue (): number {
+        return this.firstValue + 1;
+    };
+
+}
+
+export class secondAttribute {
+    constructor (
+        private secondValue: number,
+    ) {};
+
+    public returningotherValue (): number {
+        return this.secondValue * 2;
+    };
+
+}
+
+const startFirstclass = new firstAttribute(40);
+const startSecondclass = new secondAttribute(45);
+// console.log(doingMath(startFirstclass.returningValue(), startSecondclass.returningotherValue()));
+
+function numbersArray (mainTarget: number []): number {
+    return mainTarget
+    .toString()
+    .split('')
+    .map(ourValue => parseInt(ourValue, 10))
+    .reduce((ac, value) => ac + value, 0);
+}
+
+export class givingNumbers {
+    constructor (
+        private nums: number [],
+    ) {};
+
+    returnNumbersArray (): number [] {
+        return this.nums;
+    };
+
+}
+
+const newArray = new givingNumbers([7832748]);
+// console.log(numbersArray(newArray.returnNumbersArray()));
 
 export default 1;
