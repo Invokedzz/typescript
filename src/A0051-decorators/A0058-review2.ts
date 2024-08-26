1dd96d065464de51e468a1ad46b5f2192723dcba
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-26 15:36:52 -0300

    end

---
+++ src/A0051-decorators/A0058-review2.ts
@@ -0,0 +1,20 @@
+function idontknow (target: any) {
+    if (!target) return;
+    console.log(`Modifying the class: ${target.name}`);
+}
+ 
+@idontknow
+export class Person {  
+
+    constructor (
+        public name: string,
+    ) {};
+
+    returnName (): string {
+        return this.name;
+    };
+
+}
+
+const startAll = new Person('Pablo Honey');
+console.log(startAll.returnName());
