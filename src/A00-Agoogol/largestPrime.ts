7d3cfa02cb4ba2aa23d0ffe74a6a0259fc956f52
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-27 09:16:46 -0300

    att

--- src/A00-Agoogol/largestPrime.ts
+++ src/A00-Agoogol/largestPrime.ts
@@ -2,18 +2,6 @@
 
 export class findNumber {
 
-    private findPrime (num: number): boolean {
-
-        if (num <= 1) return false;
-        if (num === 2) return true;
-        if (num % 2 === 0) return false;
-        for (let i = 3; i <= Math.sqrt(num); i += 2) {
-            if (num % i === 0) return false;
-        };
-
-        return true;
-    };
-
     public getLargestPrime (num: number) {
     
         while (num % 2 === 0) num /= 2;
